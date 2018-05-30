d66a1a71aca97809d63e2bf143f114f63b634f14
Cameron Pavey <cpave3@gmail.com>
2018-05-30 23:24:10 +1000

    :construction: Foundation code for settings file processing
      - ADD: Config file loading
      - ADD: Config directory remembering
      - ADD: Code to support loading and reading config files

---
+++ src/configManagement.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const path      = require('path');
+const fs        = require('fs');
+const chalk     = require('chalk');
+
+const methods = {
+    addConfig: (configFile) => {
+        console.log(configFile);
+    },
+    defaultPreferences: () => {
+        const homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+        const defaultSettingsDir = path.join(homeDir, '.nlog', 'conf.d');
+        return {
+            config: {
+                dir: defaultSettingsDir
+            }
+        }
+    },
+    loadConfigs: (configDir) => {
+       return new Promise((resolve, reject) => { 
+            if (!fs.existsSync(configDir)){
+                fs.mkdirSync(configDir);
+            }
+
+            fs.readdir(configDir, (err, items) => {
+                if (err) {
+                    reject(err);
+                }
+                // No massive errors, so iterate through the config files and make sure they are valid
+                if (items && items.length > 0) {
+                    const arrayConfigs = items.map((item) => {
+                        return JSON.parse(fs.readFileSync(path.join(configDir, item), 'utf8'));
+                    });
+                    resolve(arrayConfigs);
+                } else {
+                    const arrayConfigs = [];
+                    resolve(arrayConfigs);
+                }
+            });
+        })
+    }
+};
+
+module.exports = methods;
---
+++ src/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Define consts
+const   connection      = new(require('nosqlite').Connection)();
+const   port            = process.env.PORT || 4001;
+const   express         = require('express'),
+        http            = require('http'),
+        chalk           = require('chalk'),
+        prettyjson      = require('prettyjson'),
+        socketIo        = require('socket.io');
+const   program         = require('commander');
+const   pjson           = require('../package.json');
+const   Preferences     = require('preferences');
+
+const { addConfig, defaultPreferences, loadConfigs }     = require('./configManagement');
+
+const prefs = new Preferences('com.bytedriven.nlog', defaultPreferences());
+
+// FIrst of all, we need to load in any config files from the config directory
+// These will be the files which specify which logs to watch and what to do with them
+let configs = [];
+loadConfigs(prefs.config.dir)
+    .then((arrayConfigs) => {
+        configs = arrayConfigs;
+    })
+    .then(() => {
+        // Now the we hopefully have some configs, we should process them and start watching files as requested
+        // TODO: Create config processor
+    })
+    .catch((err) => {
+        console.log(chalk.bgRed(`Error: ${err}`));
+    });
+
+
+program
+    .version(pjson.version || '0.0.1')
+    .description(pjson.description || '')
+    .action(() => {
+        console.log('nothing');
+    });
+
+program
+    .command('view:pref')
+    .action(() => {
+        console.log(prettyjson.render(prefs));
+    });
+
+program
+    .command('add <config>')
+    .alias('a')
+    .description('Add a new cofig file to watch')
+    .action((config) => addConfig(config));
+
+program.parse(process.argv);
